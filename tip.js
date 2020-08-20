96d69594cdefd74ecb2c13326ac3d6d9b641261c
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-08-20 11:25:37 +0200

    update.js
    practised showItemList,DeleteItemList,DeleteAllItems in list, call method in method in addItem and more

--- tip.js
+++ tip.js
@@ -1,35 +1,89 @@
 let toDoList =[];
+let toDoList2 = [];
 
 
 
 function addiItem(array){
-        let answer = prompt("sta radis ti danas govedo raspalo ?");
-        let correct = answer.toUpperCase().trim();
-        
-        if(checkDuplicateItemsInList(array,correct)){
+     let answer = prompt('Sta ubacujes u listu ');
+     let correct = answer.toLowerCase().trim();
+     
+     if(checkDuplicateItemsInList(array,correct)){
+         array.push(correct);
+         showItemList(toDoList);
+     }else{
+         alert('To vec postoji brt moj')
+     }
 
-            array.push(correct);
-        }
-        else{
-            alert('Postoji vec taj u listi ne mozes da ubacujes ponovo');
-        }
-        
+    }
+
+
+
+     //function addItem2(array2){
+    //    let answer = prompt('Koje stavke ubacujes u listu da bi kasnije spojio sa listom drugom');
+    //    let correct = answer.toLowerCase().trim();
+   //      if(checkDuplicateItemsInList(array2,correct)){
+           
+   //      array2.push(correct);
+         
+  //   }
+  //   else{
+         //alert('To vec postoji brt moj')
+  //   }
+
+  //   }
+
+
+
+
+     //function spojiDveListe(list1,list2){
+       //   let newList = list1.concat(list2);
+       //   console.log('Eve novih spojenih itema u listi ' +newList);
+
+   //  }
+
+
+
+
+
+   // this is to delete specific item in list in function
+   function removeItemSpecific(array){
+
+   let answer = prompt('Koji specificni item zelis obrisati iz lsita');
+   let correct = answer.toLowerCase().trim();
+
+   let index = array.indexOf(correct);
+   if(!checkDuplicateItemsInList(array,correct)){// znaci samo da obrnes u funckiji true false u checkDuplicateItems funkciji jer ti vraca false ako postoji stavka u listi
+       let removed = array.splice(index,1);
+       console.log(removed);
+       alert(`Obrisao si "${removed}" ovog lika iz liste koju si unosio preko prompta  `);
+   } 
+   else{
+       alert('PRobaj ponovo')
+   }
 }
-function removeItemList(array){
-      let answer = prompt('Koji item zelis obrisati jadnice jedan ? ')
-      let correct = answer.toLowerCase().trim();
 
-      let index = array.indexOf(correct);
 
-      if(checkDuplicateItemsInList(array,correct)){
-          let removedItem = array.splice(index,1);
-          console.log(removedItem);
-          alert('You removed an item from list')
-      }
-      else{
-          console.log("nema te vrednosti");
-      }
+
+
+function deleteAllItems(list){
+   for(let i = 0;i<list.length;i++){
+             if(list.length>0){
+                let rezultatList = list.pop();
+                console.log("Obriso si sve elemente u ovoj listi " +rezultatList);
+                
+             }
+   }
+
 }
+function deleteItemsFromList(list){
+    if(list.length>0){
+        console.log(`Obrisao si "${list[list.length-1]}" zadnjeg sa liste `);
+        list.pop();
+    }else{
+        console.log("nema vise nista u listi");
+    }
+}
+
 
 
 function checkDuplicateItemsInList(array,item){
@@ -44,12 +98,12 @@ function checkDuplicateItemsInList(array,item){
 }
 
 function showItemList(list){
-    let listValues = 'Your list items are : ';
-
-    for(let i = 0; i<=list.length; i++){
-        listValues = `'List item Nr ${i+1} : ${list[i]}`
-    }
-    alert(listValues);
+         let valuesFromList = "Your values are : ";
+      for(let i = 0; i<list.length; i++){
+         valuesFromList += ` " ${i} and  ${list[i]}  "  `;    
+         
+      }
+      alert(valuesFromList);
 }
 
 
@@ -57,7 +111,15 @@ function showItemList(list){
 addiItem(toDoList);
 addiItem(toDoList);
 addiItem(toDoList);
-removeItemList(toDoList);
+
+removeItemSpecific(toDoList);
+//deleteItemsFromList(toDoList);
+//addItem2(toDoList2);
+//addItem2(toDoList2);
+
+//spojiDveListe(toDoList,toDoList2);
+
+//removeItemList(toDoList);
 showItemList(toDoList);
 
-console.log(toDoList)
+console.log(toDoList);
